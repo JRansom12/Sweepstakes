7d9f93c01ff4fd45e09721dd08e81062d1155747
Jon Ransom <jransom12@live.com>
2019-08-21 14:14:47 -0500

    Factory class
    ChooseManager method returns an ISweepstakesManager through a switchcase

---
+++ Sweepstakes/Factory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Factory
+    {
+        public ISweepstakesManager ChooseManager()
+        {
+            ISweepstakesManager manager = null;
+            Console.WriteLine("Please choose a stack or queue sweepstakes manager. Enter 'stack or 'queue'");
+            string userInput = Console.ReadLine();
+            switch (userInput)
+            {
+                case "stack":
+                    manager = new SweepstakesStackManager();
+                    return manager;
+                case "queue":
+                    manager = new SweepstakesQueueManager();
+                    return manager;
+                default:
+                    ChooseManager();
+                    break;
+            }
+            return manager;
+        }
+    }
+}
--- Sweepstakes/MarketingFirm.cs
+++ Sweepstakes/MarketingFirm.cs
@@ -18,26 +18,9 @@ namespace Sweepstakes
             Sweepstakes fordRanger = new Sweepstakes();
             Sweepstakes tokyoMarathon = new Sweepstakes();
             Sweepstakes treeFiddy = new Sweepstakes();
-
         }
 
         // member methods
-        public void ChooseManager()
-        {
-            Console.WriteLine("Please choose a stack or queue sweepstakes manager. Enter 'stack or 'queue'");
-            string manager = Console.ReadLine();
-            switch (manager)
-            {
-                case "stack":
-
-                    break;
-                case "queue":
-
-                    break;
-                default:
-                    ChooseManager();
-                    break;
-            }
-        }
+        
     }
 }
